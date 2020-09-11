51317a196853a8fbd425e14dc774323ef1c42c31
prajwol <prajwol.tiwari@gmail.com>
2020-09-11 17:58:40 +0545

    Fourth Commit - Audio Focus added. - Touch feedback added for List items.

--- app/src/main/java/ssjprajwol/com/Colors.java
+++ app/src/main/java/ssjprajwol/com/Colors.java
@@ -1,5 +1,7 @@
 package ssjprajwol.com;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +12,40 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class Colors extends AppCompatActivity {
+        private MediaPlayer mMediaPlayer;
+        private AudioManager mAudioManager;
+
+        private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                        releaseMediaPlayer();
+                }
+        };
+
+        AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                        if(focusChange == AudioManager.AUDIOFOCUS_GAIN){
+                                mMediaPlayer.start();
+                        }
+                        else if(focusChange ==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                                mMediaPlayer.pause();
+                                mMediaPlayer.seekTo(0);
+                        }
+                        else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
+                                releaseMediaPlayer();
+                        }
+                }
+        };
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.word_list);
+
+                mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
 
                 final ArrayList<Word> words = new ArrayList<Word>();
 
@@ -40,16 +71,56 @@ public class Colors extends AppCompatActivity {
 
                 ListView listView = (ListView) findViewById(R.id.list);
 
+                listView.setAdapter(adapter);
+
+
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                                 Word word= words.get(i);
-                                MediaPlayer mMediaPlayer = MediaPlayer.create(Colors.this, word.getAudioResourceId());
+
+                                releaseMediaPlayer();
+
+
+                                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                                        // Use the music stream.
+                                        AudioManager.STREAM_MUSIC,
+                                        // Request permanent focus.
+                                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                                        // Start playback
+                                mMediaPlayer = MediaPlayer.create(Colors.this, word.getAudioResourceId());
                                 mMediaPlayer.start();
+                                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                        }
                         }
                 });
 
-                listView.setAdapter(adapter);
 
         }
+
+        @Override
+        protected void onStop() {
+                super.onStop();
+                releaseMediaPlayer();
+        }
+        /**
+         * Clean up the media player by releasing its resources.
+         */
+        private void releaseMediaPlayer() {
+                // If the media player is not null, then it may be currently playing a sound.
+                if (mMediaPlayer != null) {
+                        // Regardless of the current state of the media player, release its resources
+                        // because we no longer need it.
+                        mMediaPlayer.release();
+
+                        // Set the media player back to null. For our code, we've decided that
+                        // setting the media player to null is an easy way to tell that the media player
+                        // is not configured to play an audio file at the moment.
+                        mMediaPlayer = null;
+                        mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+                }
+        }
+
 }
--- app/src/main/java/ssjprajwol/com/FamilyMembers.java
+++ app/src/main/java/ssjprajwol/com/FamilyMembers.java
@@ -1,5 +1,7 @@
 package ssjprajwol.com;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,44 +12,108 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class FamilyMembers extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange == AudioManager.AUDIOFOCUS_GAIN){
+                mMediaPlayer.start();
+            }
+            else if(focusChange ==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            }
+            else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
+                releaseMediaPlayer();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         final ArrayList<Word> words = new ArrayList<Word>();
 
         words.add(new Word("hajurbuwa", "Grandfather", R.drawable.family_father, R.raw.family_grandfather));
-        words.add(new Word("hajurāmā", "Grandmother ",R.drawable.family_grandmother,R.raw.family_grandmother));
-        words.add(new Word("bābā", "Father ",R.drawable.family_father,R.raw.family_father));
-        words.add(new Word("āmā", "Mother",R.drawable.family_mother,R.raw.family_mother));
-        words.add(new Word("dāi", "Elder brother",R.drawable.family_older_brother,R.raw.family_elderbrother));
-        words.add(new Word("bhāi", "Younger brother",R.drawable.family_younger_brother,R.raw.family_youngerbrother));
-        words.add(new Word("didī", "Elder sister",R.drawable.family_older_sister,R.raw.family_eldersister));
-        words.add(new Word("bahinī", "Younger sister",R.drawable.family_younger_sister,R.raw.family_youngersister));
-        words.add(new Word("chorī", "Daughter",R.drawable.family_daughter,R.raw.family_daughter));
-        words.add(new Word("chorā", "Son",R.drawable.family_son,R.raw.family_son));
+        words.add(new Word("hajurāmā", "Grandmother ", R.drawable.family_grandmother, R.raw.family_grandmother));
+        words.add(new Word("bābā", "Father ", R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("āmā", "Mother", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("dāi", "Elder brother", R.drawable.family_older_brother, R.raw.family_elderbrother));
+        words.add(new Word("bhāi", "Younger brother", R.drawable.family_younger_brother, R.raw.family_youngerbrother));
+        words.add(new Word("didī", "Elder sister", R.drawable.family_older_sister, R.raw.family_eldersister));
+        words.add(new Word("bahinī", "Younger sister", R.drawable.family_younger_sister, R.raw.family_youngersister));
+        words.add(new Word("chorī", "Daughter", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("chorā", "Son", R.drawable.family_son, R.raw.family_son));
 
 
         /*TextView wordView = new TextView(this);
         wordView.setText(words.get(0));
         rootView.addView(wordView);*/
 
-        WordAdapter adapter = new WordAdapter(this, words,R.color.category_family);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_family);
 
         ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Word word= words.get(i);
-                MediaPlayer mMediaPlayer = MediaPlayer.create(FamilyMembers.this, word.getAudioResourceId());
-                mMediaPlayer.start();
+                releaseMediaPlayer();
+
+                Word word = words.get(i);
+
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request permanent focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Start playback
+                    mMediaPlayer = MediaPlayer.create(FamilyMembers.this, word.getAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
-
-        listView.setAdapter(adapter);
-
     }
-}
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+
+        /**
+         * Clean up the media player by releasing its resources.
+         */
+        private void releaseMediaPlayer() {
+            // If the media player is not null, then it may be currently playing a sound.
+            if (mMediaPlayer != null) {
+                // Regardless of the current state of the media player, release its resources
+                // because we no longer need it.
+                mMediaPlayer.release();
+
+                // Set the media player back to null. For our code, we've decided that
+                // setting the media player to null is an easy way to tell that the media player
+                // is not configured to play an audio file at the moment.
+                mMediaPlayer = null;
+            }
+        }
+    }
